65de71fb3bf1f3ebb6bda7361002f86c795735a4
Qing Ma <q.ma@ncontrol.cloud>
2021-04-14 11:47:01 +0200

    Add project files.

---
+++ DemoCode.Tests/NumberDemos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DemoCode.Tests
+{
+    public class NumberDemos
+    {
+        [Fact]
+        public void Ints()
+        {
+            // arrange
+            var sut = new IntCalculator();
+
+            // act
+            sut.Subtract(1);
+
+            // assert
+            Assert.True(sut.Value < 0);
+        }
+    }
+}
