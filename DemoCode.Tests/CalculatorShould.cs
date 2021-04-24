3d42371290c8ed2b42e6ec8d5fec106cdd3a2c62
Qing Ma <q.ma@ncontrol.cloud>
2021-04-24 22:42:27 +0200

    Test with InlineData

---
+++ DemoCode.Tests/CalculatorShould.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using AutoFixture;
+
+namespace DemoCode.Tests
+{
+    public class CalculatorShould
+    {
+        [Theory]
+        [InlineData(1, 2)] // AddTwoPositiveNumbers
+        [InlineData(0, 2)] // AddZeroAndPositiveNumbers
+        [InlineData(-5, 1)] // AddNegativeAndPositiveNumbers
+        public void AddNumbers(int a, int b)
+        {
+            var sut = new Calculator();
+
+            sut.Add(a);
+            sut.Add(b);
+
+            Assert.Equal(a + b, sut.Value);
+        }
+    }
+}
