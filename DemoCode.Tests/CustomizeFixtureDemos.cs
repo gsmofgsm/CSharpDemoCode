f370ee579a97730546f59ecafa03c75dc1ea02cf
Qing Ma <q.ma@ncontrol.cloud>
2021-04-24 17:14:49 +0200

    Customizing Values generated in a fixture instance

---
+++ DemoCode.Tests/CustomizeFixtureDemos.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using AutoFixture;
+
+namespace DemoCode.Tests
+{
+    public class CustomizeFixtureDemos
+    {
+        [Fact]
+        public void SettingValue()
+        {
+            // arrange
+            var fixture = new Fixture();
+
+            fixture.Inject<string>("LHR");
+
+            var flight = fixture.Create<FlightDetails>();
+
+            // etc.
+        }
+
+        [Fact]
+        public void SettingValueForCustomType()
+        {
+            // arrange
+            var fixture = new Fixture();
+
+            fixture.Inject(new FlightDetails
+            {
+                DepartureAirportCode = "PER",
+                ArrivalAirportCode = "LHR",
+                FlightDuration = TimeSpan.FromHours(10),
+                AirlineName = "Awesome Aero"
+            });
+
+            var flight1 = fixture.Create<FlightDetails>();
+            var flight2 = fixture.Create<FlightDetails>();
+
+            // etc
+        }
+
+        [Fact]
+        public void CustomCreationFunction()
+        {
+            // arrange
+            var fixture = new Fixture();
+
+            fixture.Register<string>(() => DateTime.Now.Ticks.ToString()); 
+
+            var string1 = fixture.Create<string>();
+            var string2 = fixture.Create<string>();
+
+            // etc.
+        }
+    }
+}
---
+++ DemoCode/FlightDetails.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoCode
+{
+    public class FlightDetails
+    {
+        private string _departureAirportCode;
+        private string _arraivalAirportCode;
+
+        public string DepartureAirportCode
+        {
+            get => _departureAirportCode;
+            set
+            {
+                EnsureValidAirportCode(value);
+                _departureAirportCode = value;
+            }
+        }
+
+        public string ArrivalAirportCode
+        {
+            get => _arraivalAirportCode;
+            set
+            {
+                EnsureValidAirportCode(value);
+                _arraivalAirportCode = value;
+            }
+        }
+
+        public TimeSpan FlightDuration { get; set; }
+        public string AirlineName { get; set; }
+        public List<string> MealOptions { get; set; } = new List<string>();
+
+        private void EnsureValidAirportCode(string airportCode)
+        {
+            var isWrongLength = airportCode.Length != 3;
+
+            var isWrongCase = airportCode != airportCode.ToUpperInvariant();
+
+            if (isWrongLength || isWrongCase)
+            {
+                throw new Exception(airportCode + " is an invalid airport");
+            }
+        }
+    }
+}
