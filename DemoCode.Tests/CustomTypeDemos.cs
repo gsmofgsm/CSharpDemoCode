d8c3fb10c5f2b4ae42dda7405a6c0832a49dee52
Qing Ma <q.ma@ncontrol.cloud>
2021-04-24 15:03:05 +0200

    Anonymous Instance of custom types

---
+++ DemoCode.Tests/CustomTypeDemos.cs
@@ -0,0 +1,48 @@
+﻿using AutoFixture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DemoCode.Tests
+{
+    public class CustomTypeDemos
+    {
+        [Fact]
+        public void ManualCreation()
+        {
+            // arrange
+            var sut = new EmailMessageBuffer();
+
+            EmailMessage message = new EmailMessage("sarah@dontcodetired.com",
+                                                    "Hello, hope you are well, Jason",
+                                                    false);
+
+            message.Subject = "Hi";
+
+            // act
+            sut.Add(message);
+
+            // assert
+            Assert.Single(sut.Emails);
+        }
+
+        [Fact]
+        public void AutoCreation()
+        {
+            // arrange
+            var fixture = new Fixture();
+            var sut = new EmailMessageBuffer();
+
+            EmailMessage message = fixture.Create<EmailMessage>();
+
+            // act
+            sut.Add(message);
+
+            // assert
+            Assert.Single(sut.Emails);
+        }
+    }
+}
---
+++ DemoCode/EmailMessageBuffer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoCode
+{
+    public class EmailMessageBuffer
+    {
+        public List<EmailMessage> Emails { get; private set; } = new List<EmailMessage>();
+
+        public void SendAll()
+        {
+            foreach (var email in Emails)
+            {
+                // Send email
+            }
+        }
+
+        public void Add(EmailMessage message)
+        {
+            Emails.Add(message);
+        }
+    }
+}
