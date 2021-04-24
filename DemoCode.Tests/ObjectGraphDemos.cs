f6ac00898b212d0313a32073920f881e0ed8b3af
Qing Ma <q.ma@ncontrol.cloud>
2021-04-24 15:10:24 +0200

    Complex anonymous object graph

---
+++ DemoCode.Tests/ObjectGraphDemos.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+using AutoFixture;
+
+namespace DemoCode.Tests
+{
+    public class ObjectGraphDemos
+    {
+        [Fact]
+        public void ManualCreation()
+        {
+            // quite large
+        }
+
+        [Fact]
+        public void AutoCreation()
+        {
+            // arrange
+            var fixture = new Fixture();
+
+            Order order = fixture.Create<Order>();
+
+            // act and assert phases...
+        }
+    }
+}
---
+++ DemoCode/Order.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoCode
+{
+    public class Order
+    {
+        public Order(Customer customer)
+        {
+            Customer = customer;
+            Items = new List<OrderItem>();
+        }
+
+        public int Id { get; set; }
+        public Customer Customer { get; set; }
+        public List<OrderItem> Items { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+
+    public class OrderItem
+    {
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+    }
+    public class Customer
+    {
+        public string CustomerName { get; set; }
+    }
+}
