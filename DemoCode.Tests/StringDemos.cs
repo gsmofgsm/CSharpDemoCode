f53bd912a2206a0421a2b4e7aa429d9fea738bf4
Qing Ma <q.ma@ncontrol.cloud>
2021-04-24 12:53:11 +0200

    Fixture with anonymous strings

---
+++ DemoCode.Tests/StringDemos.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using AutoFixture;
+
+namespace DemoCode.Tests
+{
+    public class StringDemos
+    {
+        [Fact]
+        public void BasicStrings()
+        {
+            // arrange
+            var fixture = new Fixture();
+            var sut = new NameJoiner();
+
+            //var firstName = fixture.Create<string>();
+            //var lastName = fixture.Create<string>();
+            var firstName = fixture.Create<string>("First_");
+            var lastName = fixture.Create<string>("Last_");
+
+            // act
+            var result = sut.Join(firstName, lastName);
+
+            // assert
+            Assert.Equal(firstName + ' ' + lastName, result);
+        }
+
+        [Fact]
+        public void Chars()
+        {
+            // arrange
+            var fixture = new Fixture();
+
+            var anonChar = fixture.Create<char>();
+
+            // etc.
+        }
+    }
+}
---
+++ DemoCode/NameJoiner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoCode
+{
+    public class NameJoiner
+    {
+        public int MyProperty { get; set; }
+
+        public string Join(string firstName, string lastName)
+        {
+            return firstName + ' ' + lastName;
+        }
+    }
+}
