cab54f5392601c0c23e8dfbabc805011d1016a03
Qing Ma <q.ma@ncontrol.cloud>
2021-04-24 22:49:12 +0200

    [InlineAutoData]

--- DemoCode.Tests/CalculatorShould.cs
+++ DemoCode.Tests/CalculatorShould.cs
@@ -7,13 +7,11 @@ namespace DemoCode.Tests
     public class CalculatorShould
     {
         [Theory]
-        [InlineData(1, 2)] // AddTwoPositiveNumbers
-        [InlineData(0, 2)] // AddZeroAndPositiveNumbers
-        [InlineData(-5, 1)] // AddNegativeAndPositiveNumbers
-        public void AddNumbers(int a, int b)
+        [InlineAutoData] // AddTwoPositiveNumbers
+        [InlineAutoData(0)] // AddZeroAndPositiveNumbers
+        [InlineAutoData(-5)] // AddNegativeAndPositiveNumbers
+        public void AddNumbers(int a, int b, Calculator sut)
         {
-            var sut = new Calculator();
-
             sut.Add(a);
             sut.Add(b);
 
