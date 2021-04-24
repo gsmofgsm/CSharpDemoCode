39b92cadca04cba79f637bbd5239c79c62805edd
Qing Ma <q.ma@ncontrol.cloud>
2021-04-24 13:03:50 +0200

    Anonymous Date and Times

---
+++ DemoCode.Tests/DateAndTimeDemos.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using AutoFixture;
+
+namespace DemoCode.Tests
+{
+    public class DateAndTimeDemos
+    {
+        [Fact]
+        public void DateTimes()
+        {
+            // arrange
+            var fixture = new Fixture();
+            //DateTime logTime = new DateTime(2020, 1, 21);
+            DateTime logTime = fixture.Create<DateTime>();
+
+            // act
+            LogMessage result = LogMessageCreator.Create(fixture.Create<string>(), logTime);
+
+            // assert
+            Assert.Equal(logTime.Year, result.Year);
+        }
+
+        [Fact]
+        public void TimeSpans()
+        {
+            // arrange
+            var fixture = new Fixture();
+
+            TimeSpan logTime = fixture.Create<TimeSpan>();
+
+            // etc
+        }
+    }
+}
---
+++ DemoCode/LogMessageCreator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoCode
+{
+    public static class LogMessageCreator
+    {
+        public static LogMessage Create(string message, DateTime when)
+        {
+            return new LogMessage
+            {
+                Year = when.Year,
+                Message = message
+            };
+        }
+    }
+}
