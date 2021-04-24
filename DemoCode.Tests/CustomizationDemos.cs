8dd581d7aab135bcc596cadc99c8bc64ed8902b0
Qing Ma <q.ma@ncontrol.cloud>
2021-04-24 19:49:24 +0200

    Prebuild Customization

---
+++ DemoCode.Tests/CustomizationDemos.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+using AutoFixture;
+using System;
+
+namespace DemoCode.Tests
+{
+    public class CustomizationDemos
+    {
+        [Fact]
+        public void DateTimeCustomization()
+        {
+            // arrange
+            var fixture = new Fixture();
+
+            //fixture.Customize(new CurrentDateTimeCustomization()); // prebuild Customization
+            fixture.Customizations.Add(new CurrentDateTimeGenerator());
+
+            var date1 = fixture.Create<DateTime>();
+            var date2 = fixture.Create<DateTime>();
+
+            // etc.
+        }
+    }
+}
