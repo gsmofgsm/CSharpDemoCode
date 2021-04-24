e75b78ca6078a527695beee0b324eed29135f8d9
Qing Ma <q.ma@ncontrol.cloud>
2021-04-24 13:12:58 +0200

    Guid and Enums

---
+++ DemoCode/EmailMessage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoCode
+{
+    public class EmailMessage
+    {
+        private string _somePrivateField;
+        public string SomePublicField;
+
+        public EmailMessage(string toAddress, string messageBody, bool isImportant)
+        {
+            ToAddress = toAddress;
+            MessageBody = messageBody;
+            IsImportant = isImportant;
+        }
+
+        public Guid Id { get; set; }
+
+        public string ToAddress { get; set; }
+        public string MessageBody { get; set; }
+        public string Subject { get; set; }
+        public bool IsImportant { get; set; }
+
+        public EmailMessageType MessageType { get; set; }
+        private string SomePrivateProperty { get; set; }
+    }
+}
