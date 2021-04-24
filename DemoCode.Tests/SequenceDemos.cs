23c968a11ca7d7b5d4a65422b3f9e527984e0e5a
Qing Ma <q.ma@ncontrol.cloud>
2021-04-24 13:26:24 +0200

    AddManyTo

---
+++ DemoCode.Tests/SequenceDemos.cs
@@ -0,0 +1,65 @@
+﻿using AutoFixture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DemoCode.Tests
+{
+    public class SequenceDemos
+    {
+        [Fact]
+        public void SequenceOfStrings()
+        {
+            // arrange
+            var fixture = new Fixture();
+
+            IEnumerable<string> messages = fixture.CreateMany<string>();
+
+            // etc
+        }
+
+        [Fact]
+        public void ExplicitNumberOfItems()
+        {
+            // arrange
+            var fixture = new Fixture();
+
+            IEnumerable<int> numbers = fixture.CreateMany<int>(6);
+
+            // etc
+        }
+
+        [Fact]
+        public void AddingToExistingList()
+        {
+            // arrange
+            var fixture = new Fixture();
+
+            var sut = new DebugMessageBuffer();
+
+            fixture.AddManyTo<string>(sut.Messages, 10);
+
+            // act
+            sut.WriteMessages();
+
+            // assert
+            Assert.Equal(10, sut.MessagesWritten);
+        }
+
+        [Fact]
+        public void AddingToExistingListWithCreatorFunction()
+        {
+            // arrange
+            var fixture = new Fixture();
+
+            var sut = new DebugMessageBuffer();
+
+            fixture.AddManyTo<string>(sut.Messages, () => "hi");
+
+            // etc.
+        }
+    }
+}
---
+++ DemoCode/DebugMessageBuffer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoCode
+{
+    public class DebugMessageBuffer
+    {
+        public List<string> Messages { get; set; } = new List<string>();
+
+        public int MessagesWritten { get; private set; }
+
+        public void WriteMessages()
+        {
+            foreach (var messages in Messages)
+            {
+                // Do something with message...
+                MessagesWritten++;
+            }
+        }
+    }
+}
